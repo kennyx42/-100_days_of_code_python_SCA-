4eca1066d23502a97cb1f84d94a7a2faca1ad795
kennyx42 <owosenikehinde01@gmail.com>
2021-05-02 08:01:22 +0100

    first commit

---
+++ day_1_of_code.py
@@ -0,0 +1,13 @@
+#1. Create a greeting for your program.
+greeting='Welcome to Band Name'
+
+#2. Ask the user for the city that they grew up in.
+city_name= input('Enter the name of the city you grew up in:\n')
+
+#3. Ask the user for the name of a pet.
+pet_name= input('Enter the name of your pet:\n')
+
+#4. Combine the name of their city and pet and show them their band name.
+band_name= (city_name+' '+ pet_name)
+
+print(band_name)
